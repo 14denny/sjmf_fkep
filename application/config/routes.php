c161770a6f65e4c0fcde932f2077236367b00597
Denny Syaputra <syaputradenny911@gmail.com>
2020-06-29 11:53:31 +0700

    Perbaikan update

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,35 +53,3 @@ $route['default_controller'] = 'index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-//AWAL: menu tentang kami
-$route['tentang/visi_misi'] = 'tentang/visi';
-$route['tentang/struktur_organisasi'] = 'tentang/struktur';
-$route['tentang/program_sasaran'] = 'tentang/program';
-$route['tentang/kegiatan'] = 'tentang/kegiatan';
-$route['tentang/sk_rektor'] = 'tentang/rektor';
-$route['tentang/kebijakan_mutu'] = 'tentang/kebijakan';
-//AKHIR: menu tentang kami
-
-//AWAL: menu pengurus
-$route['pengurus'] = 'pengurus/pengurus';
-//AKHIR: menu pengurus
-
-//AWAL: menu produk
-$route['produk'] = 'produk/produk';
-//AKHIR: menu produk
-
-//AWAL: menu akreditasi
-$route['akreditasi'] = 'akreditasi/akreditasi';
-//AKHIR: menu akreditasi
-
-//AWAL: menu download
-$route['download'] = 'download/download';
-//AKHIR: menu download
-
-//AWAL: menu galeri foto
-$route['galeri_foto'] = 'galeri/galeri_foto';
-//AKHIR: menu galeri foto
-
-//AWAL: menu kontak
-$route['kontak'] = 'kontak/kontak';
-//AKHIR: menu kontak
--- index.php
+++ index.php
@@ -1,44 +1,315 @@
-<!-- BEGIN: Page Main-->
-<div id="main">
-	<div class="row">
-		<div class="col s12">
-			<div class="container">
-				<div class="section">
-					<!-- INI BATAS AWAL SLIDER -->
-					<div class="row">
-						<div class="col s12">
-							<div class="slider">
-								<ul class="slides">
-									<?php foreach ($slider_images as $image) { ?>
-										<li>
-											<img src="<?php echo $image->location ?>"> <!-- random image -->
-											<div class="caption <?php echo ($image->posisi == 1 ? 'left' : ($image->posisi == 2 ? 'center' : '')) ?>-align">
-												<h3 class="light grey-text text-<?php echo ($image->warna_txt_1 == 0 ? "darken" : "lighten") ?>-3"><?php echo $image->teks_1 ?></h3>
-												<h5 class="light grey-text text-<?php echo ($image->warna_txt_2 == 0 ? "darken" : "lighten") ?>-3"><?php echo $image->teks_2 ?></h5>
-												<!-- <p class="light grey-text text-darken-3">Pilih menu diatas untuk...</p> -->
-											</div>
-										</li>
-									<?php } ?>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<!-- INI BATAS AKHIR SLIDER -->
-				</div>
-				<div class="container" style="padding-top: 30px; padding-bottom: 30px">
-					<div class="col s8">
-						Pentingnya pengawasan mutu telah diisyaratkan oleh Pemerintah sejak tahun 1990 seperti yang tertera dalam PP No. 30 thn 1990 tentang Pendidikan Tinggi pasal 121. Badan Penjaminan Mutu Akademik (BPMA) merupakan badan internal UI yang berfungsi membangun sistem penjaminan mutu akademik di UI pada setiap aras di UI mulai dari aras program studi, departemen, fakultas hingga direktorat/unit kerja di lingkungan Pusat Administrasi UI. BPMA berawal dari berdirinya Badan Audit Akademik (BAA) yang merupakan salah satu kelengkapan khusus tingkat universitas sebagaimana tertera dalam statuta UI 1992 pasal 21. BAA berdiri pada tahun 2002 melalui surat keputusan Rektor UI Nomor 123/SK/R/UI/2000 yang mengatur tentang Struktur Organisasi dan Tatalaksana Badan Audit Akademik UI.
-						<br><br>Namun setelah dilakukan kajian yang mendalam mengenai penjaminan mutu dan audit mutu akademik, serta kewajiban mengemban tugas yang lebih luas, maka disepakati untuk mengubah nama Badan Audit Akademik menjadi Badan Penjaminan Mutu Akademik sejak tahun 2004, melalui SK Rektor UI No. 012/SK/UI/2004 tentang Struktur Pelaksana Penjaminan Mutu Akademik Universitas Indonesia. Fungsi BPMA UI dipertegas dalam Peraturan Pemerintah No. 68 tahun 2013 tentang Statuta UI, Pasal 52 ayat 3 dan 4.
-						<br><br>Sistem Penjaminan Mutu Internal UI telah diatur dalam SK Rektor no. 1421/SK/R/UI/2010 tentang Kebijakan Sistem Penjaminan Mutu Akademik di Universitas Indonesia, yang diperbaharui dengan Peraturan Rektor No. 009 Tahun 2016 tentang Sistem Penjaminan Mutu Internal UI.
-						<br><br>Dalam rangka meningkatkan kinerja layanan penjaminan mutu, BPMA telah menerapkan Quality Management System, dan memperoleh sertikat ISO 9001:2008 dari PT. DQS Sertification Indonesia pada bulan Februari 2013, dan re-sertifikasi ISO 9001:2015 pada bulan Juli 2018.
-					</div>
-					<div class="col s4">
-						ini sideview nya
-					</div>
-				</div>
-			</div>
-			<div class="content-overlay"></div>
-		</div>
-	</div>
-</div>
-<!-- END: Page Main-->
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2019, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
+ * @license	https://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+
+/*
+ *---------------------------------------------------------------
+ * APPLICATION ENVIRONMENT
+ *---------------------------------------------------------------
+ *
+ * You can load different configurations depending on your
+ * current environment. Setting the environment also influences
+ * things like logging and error reporting.
+ *
+ * This can be set to anything, but default usage is:
+ *
+ *     development
+ *     testing
+ *     production
+ *
+ * NOTE: If you change these, also change the error_reporting() code below
+ */
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+
+/*
+ *---------------------------------------------------------------
+ * ERROR REPORTING
+ *---------------------------------------------------------------
+ *
+ * Different environments will require different levels of error reporting.
+ * By default development will show errors but testing and live will hide them.
+ */
+switch (ENVIRONMENT)
+{
+	case 'development':
+		error_reporting(-1);
+		ini_set('display_errors', 1);
+	break;
+
+	case 'testing':
+	case 'production':
+		ini_set('display_errors', 0);
+		if (version_compare(PHP_VERSION, '5.3', '>='))
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+		}
+		else
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+		}
+	break;
+
+	default:
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'The application environment is not set correctly.';
+		exit(1); // EXIT_ERROR
+}
+
+/*
+ *---------------------------------------------------------------
+ * SYSTEM DIRECTORY NAME
+ *---------------------------------------------------------------
+ *
+ * This variable must contain the name of your "system" directory.
+ * Set the path if it is not in the same directory as this file.
+ */
+	$system_path = 'system';
+
+/*
+ *---------------------------------------------------------------
+ * APPLICATION DIRECTORY NAME
+ *---------------------------------------------------------------
+ *
+ * If you want this front controller to use a different "application"
+ * directory than the default one you can set its name here. The directory
+ * can also be renamed or relocated anywhere on your server. If you do,
+ * use an absolute (full) server path.
+ * For more info please see the user guide:
+ *
+ * https://codeigniter.com/user_guide/general/managing_apps.html
+ *
+ * NO TRAILING SLASH!
+ */
+	$application_folder = 'application';
+
+/*
+ *---------------------------------------------------------------
+ * VIEW DIRECTORY NAME
+ *---------------------------------------------------------------
+ *
+ * If you want to move the view directory out of the application
+ * directory, set the path to it here. The directory can be renamed
+ * and relocated anywhere on your server. If blank, it will default
+ * to the standard location inside your application directory.
+ * If you do move this, use an absolute (full) server path.
+ *
+ * NO TRAILING SLASH!
+ */
+	$view_folder = '';
+
+
+/*
+ * --------------------------------------------------------------------
+ * DEFAULT CONTROLLER
+ * --------------------------------------------------------------------
+ *
+ * Normally you will set your default controller in the routes.php file.
+ * You can, however, force a custom routing by hard-coding a
+ * specific controller class/function here. For most applications, you
+ * WILL NOT set your routing here, but it's an option for those
+ * special instances where you might want to override the standard
+ * routing in a specific front controller that shares a common CI installation.
+ *
+ * IMPORTANT: If you set the routing here, NO OTHER controller will be
+ * callable. In essence, this preference limits your application to ONE
+ * specific controller. Leave the function name blank if you need
+ * to call functions dynamically via the URI.
+ *
+ * Un-comment the $routing array below to use this feature
+ */
+	// The directory name, relative to the "controllers" directory.  Leave blank
+	// if your controller is not in a sub-directory within the "controllers" one
+	// $routing['directory'] = '';
+
+	// The controller class file name.  Example:  mycontroller
+	// $routing['controller'] = '';
+
+	// The controller function you wish to be called.
+	// $routing['function']	= '';
+
+
+/*
+ * -------------------------------------------------------------------
+ *  CUSTOM CONFIG VALUES
+ * -------------------------------------------------------------------
+ *
+ * The $assign_to_config array below will be passed dynamically to the
+ * config class when initialized. This allows you to set custom config
+ * items or override any default config values found in the config.php file.
+ * This can be handy as it permits you to share one application between
+ * multiple front controller files, with each file containing different
+ * config values.
+ *
+ * Un-comment the $assign_to_config array below to use this feature
+ */
+	// $assign_to_config['name_of_config_item'] = 'value of config item';
+
+
+
+// --------------------------------------------------------------------
+// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
+// --------------------------------------------------------------------
+
+/*
+ * ---------------------------------------------------------------
+ *  Resolve the system path for increased reliability
+ * ---------------------------------------------------------------
+ */
+
+	// Set the current directory correctly for CLI requests
+	if (defined('STDIN'))
+	{
+		chdir(dirname(__FILE__));
+	}
+
+	if (($_temp = realpath($system_path)) !== FALSE)
+	{
+		$system_path = $_temp.DIRECTORY_SEPARATOR;
+	}
+	else
+	{
+		// Ensure there's a trailing slash
+		$system_path = strtr(
+			rtrim($system_path, '/\\'),
+			'/\\',
+			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+		).DIRECTORY_SEPARATOR;
+	}
+
+	// Is the system path correct?
+	if ( ! is_dir($system_path))
+	{
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
+		exit(3); // EXIT_CONFIG
+	}
+
+/*
+ * -------------------------------------------------------------------
+ *  Now that we know the path, set the main path constants
+ * -------------------------------------------------------------------
+ */
+	// The name of THIS file
+	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+
+	// Path to the system directory
+	define('BASEPATH', $system_path);
+
+	// Path to the front controller (this file) directory
+	define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
+
+	// Name of the "system" directory
+	define('SYSDIR', basename(BASEPATH));
+
+	// The path to the "application" directory
+	if (is_dir($application_folder))
+	{
+		if (($_temp = realpath($application_folder)) !== FALSE)
+		{
+			$application_folder = $_temp;
+		}
+		else
+		{
+			$application_folder = strtr(
+				rtrim($application_folder, '/\\'),
+				'/\\',
+				DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+			);
+		}
+	}
+	elseif (is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
+	{
+		$application_folder = BASEPATH.strtr(
+			trim($application_folder, '/\\'),
+			'/\\',
+			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+		);
+	}
+	else
+	{
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+		exit(3); // EXIT_CONFIG
+	}
+
+	define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
+
+	// The path to the "views" directory
+	if ( ! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
+	{
+		$view_folder = APPPATH.'views';
+	}
+	elseif (is_dir($view_folder))
+	{
+		if (($_temp = realpath($view_folder)) !== FALSE)
+		{
+			$view_folder = $_temp;
+		}
+		else
+		{
+			$view_folder = strtr(
+				rtrim($view_folder, '/\\'),
+				'/\\',
+				DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+			);
+		}
+	}
+	elseif (is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
+	{
+		$view_folder = APPPATH.strtr(
+			trim($view_folder, '/\\'),
+			'/\\',
+			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+		);
+	}
+	else
+	{
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+		exit(3); // EXIT_CONFIG
+	}
+
+	define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
+
+/*
+ * --------------------------------------------------------------------
+ * LOAD THE BOOTSTRAP FILE
+ * --------------------------------------------------------------------
+ *
+ * And away we go...
+ */
+require_once BASEPATH.'core/CodeIgniter.php';
